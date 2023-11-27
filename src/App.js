4322312db661d5268791a1f97c8bcda42ac112d4
JoseAl05 <jparancibialinker@gmail.com>
2023-11-26 22:56:58 -0300

    Modify img src

--- src/App.js
+++ src/App.js
@@ -120,7 +120,7 @@ function App() {
           </div>
         </form>
         <div className='main-section-image-container'>
-          {image.length !== 0 && <img src='https://oaidalleapiprodscus.blob.core.windows.net/private/org-gljnyerf2dca8rMI0QBFwiSe/user-gqyAxdJy0PkJGJQuIUPGLxFh/img-rulIiLGK1vBeDExmlTiHSFNX.png?st=2023-11-27T00%3A09%3A31Z&se=2023-11-27T02%3A09%3A31Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-11-27T00%3A18%3A27Z&ske=2023-11-28T00%3A18%3A27Z&sks=b&skv=2021-08-06&sig=yo5Ihz0xhq%2BBEGU8tIQ6idhSekwswpwQxtYDWGkZyYU%3D' alt='User'  />}
+          {image.length !== 0 && <img src={image} alt='User'  />}
           <div className='main-section-code-container'>
             <code className='response-data'>
               {Object.keys(data).length !== 0 ? (
